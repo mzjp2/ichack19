e33c46b86bd6167938d6a03ceab95501d3d2a675
Zain Patel <zain.patel06@gmail.com>
2019-01-26 19:25:12 +0000

    trying to send latex

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def get_message():
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
     #sends user the text message provided via input response parameter
-    bot.send_text_message(recipient_id, response)
+    bot.send_generic_message(recipient_id, response)
     return "success"
 
 if __name__ == "__main__":
