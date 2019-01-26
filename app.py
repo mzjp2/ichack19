0ff753aba1905630a6260207650cad1c92caaf3b
Zain Patel <zain.patel06@gmail.com>
2019-01-26 19:29:10 +0000

    fixed latex

--- app.py
+++ app.py
@@ -45,14 +45,14 @@ def verify_fb_token(token_sent):
 
 #chooses a random message to send to the user
 def get_message():
-    sample_responses = ["$x+2 = 3$"]
+    sample_responses = ["$$x+2 = 3$$"]
     # return selected item to the user
     return random.choice(sample_responses)
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
     #sends user the text message provided via input response parameter
-    bot.send_generic_message(recipient_id, response)
+    bot.send_text_message(recipient_id, response)
     return "success"
 
 if __name__ == "__main__":
