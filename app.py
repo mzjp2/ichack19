6de36388dbed941d72f155caebff1bf135a16fd8
Zain Patel <zain.patel06@gmail.com>
2019-01-27 07:23:10 +0000

    using globvar properly

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ class User(db.Model):
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
+    global question_prev
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
         that confirms all requests that your bot receives came from Facebook.""" 
